8dcda12cfa8efbdb79d4c7c68e5b375cf7cae23f
KimberlingOrtega <1996.gorgette21ortega@gmail.com>
2022-07-02 19:01:10 +0000

    creado y modificado el home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
 import React from "react";
-
+import Counter from "./Counter.jsx";
+import PropTypes from "prop-types";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = (props) => {
+  return (
+    <div className="text-center d-flex bg-danger align-items-center vh-100">
+      <div className="col-2">
+        <div className="card">
+          <div className="card-body">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/109/109613.png"
+              height="40px"
+            ></img>
+          </div>
+        </div>
+      </div>
+
+      <div className="col-2">
+        <Counter unidad={Math.floor((props.seconds/10000)%10)} />
+      </div>
+
+      <div className="col-2">
+        <Counter unidad={Math.floor((props.seconds/1000)%10)} />
+      </div>
+
+      <div className="col-2">
+        <Counter unidad={Math.floor((props.seconds/100)%10)} />
+      </div>
+
+      <div className="col-2">
+        <Counter unidad={Math.floor((props.seconds/10)%10)} />
+      </div>
+
+      <div className="col-2">
+        <Counter unidad={Math.floor((props.seconds/1)%10)} />
+      </div>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  seconds: PropTypes.number,
 };
 
 export default Home;
